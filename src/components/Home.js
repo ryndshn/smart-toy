d6bd7e9bcf604be3e0843c0c2bae87df420c3dee
ryandushane <ryan4888@gmail.com>
2017-09-19 09:37:31 -0400

    Add key bindings for WASD

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,15 @@ class Home extends React.Component {
     super(props);
     this.onForwardBackwardSliderChange = this.onForwardBackwardSliderChange.bind(this);
     this.onLeftRightSliderChange = this.onLeftRightSliderChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown", this.handleKeyDown, false);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.handleKeyDown, false);
   }
 
   onForwardBackwardSliderChange(value) {
@@ -23,6 +32,35 @@ class Home extends React.Component {
       value,
     });
     this.props.sendLeftRight(value, this.props.home);
+  }
+
+  handleKeyDown(event) {
+    switch(event.key) {
+      case 'w': {
+        if((this.props.home.forwardBackward + 10) <= 100) {
+          this.onForwardBackwardSliderChange(this.props.home.forwardBackward + 10);
+        }
+        break;
+      }
+      case 's': {
+        if((this.props.home.forwardBackward - 10) >= 0) {
+          this.onForwardBackwardSliderChange(this.props.home.forwardBackward - 10);
+        }
+        break;
+      }
+      case 'd': {
+        if((this.props.home.leftRight + 10) <= 100) {
+          this.onLeftRightSliderChange(this.props.home.leftRight + 10);
+        }
+        break;
+      }
+      case 'a': {
+        if((this.props.home.leftRight - 10) >= 0) {
+          this.onLeftRightSliderChange(this.props.home.leftRight - 10);
+        }
+        break;
+      }
+    }
   }
 
   render() {
@@ -48,7 +86,7 @@ class Home extends React.Component {
                 onAfterChange={this.onAfterChange}
                 vertical
                 marks={forwardBackwardMarks}
-                step={20}
+                step={10}
               />
             </div>
           </li>
@@ -59,7 +97,7 @@ class Home extends React.Component {
                 onChange={this.onLeftRightSliderChange}
                 onAfterChange={this.onAfterChange}
                 marks={leftRightMarks}
-                step={20}
+                step={10}
               />
             </div>
           </li>
@@ -73,6 +111,9 @@ class Home extends React.Component {
 Home.propTypes = {
   sendLeftRight: PropTypes.func.isRequired,
   sendForwardBackward: PropTypes.func.isRequired,
+  sendStart: PropTypes.func.isRequired,
+  sendStop: PropTypes.func.isRequired,
+  sendPlay: PropTypes.func.isRequired,
   home: PropTypes.object.isRequired
 };
 
